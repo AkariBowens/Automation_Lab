e44e9c8f5521b0135a43e28a925cd18e3474506e
Akari Bowens <akarimbowens@gmail.com>
2025-05-10 13:10:46 -0700

    Update create_users.sh

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 # Script to automate user creation
 
-LOG_FILE = "var/log/USER_LOG_FILE"
+# I did not give this a dedicated file because it is for a home lab, I would in a work-setting
+LOG_FILE = "user_log_file.log"
 
-# Create or read log file
-touch $USER_LOG_FILE
+# Checks for an input file
+if [ -z "$1"]; then 
+  echo "Usage: $0 users.txt"
+  exit 1
+fi
+
+echo "Starting user creation: $(date)" >> "$log_file"
+
+# Reads each line in file, separates last name and first name in each line by ","
+while IFS=, read -r last first; do
+
+# Removes trailing and leading space
+  first=$(echo "$first" | xargs)
+  last=$(echo "$last" | xargs)
+
+# Create a lowercase username with the first initial + last name
+  username=$(echo "${first:0:1}${last}" | tr '[:upper:]' '[:lower:]')
+
+# Checks if the username already exists
+  if id "$username" &>/dev/null; then 
+    echo "User $username already exists. Skipping" >> "$log_file"
+  else 
+    #Creates user and home directory
+    sudo useradd -m "$username"
+    echo "User $username created."
+
+    # Set default password
+    echo "$username:password123" | sudo chpasswd
+    echo "Set default password for $username." >> "$log_file"
+  fi
+done < "$1"
+
+echo "User creation finished: $(date)" >> "$log_file"
